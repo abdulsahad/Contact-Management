88cc95e90560e7bde152444ce07c10b8df8ae78c
Abdul Ahad Abid <ch.abdulsahad@gmail.com>
2025-12-05 02:26:45 +0000

    Just Created a Menu page and also create a function to store contacts

--- app.py
+++ app.py
@@ -1,9 +1,5 @@
-# Contact Management App (Start)
-# Author: Abdul Ahad
-# This is the very basic structure of the program.
-
 def show_menu():
-    print("\n===== Contact Management App =====")
+    print("\n=====// Contact Management App //=====")
     print("1. Add Contact")
     print("2. View Contacts")
     print("3. Search Contact")
@@ -11,17 +7,34 @@ def show_menu():
     print("5. Exit")
 
 def main():
-    contacts = []  # list to store all contacts
+    contacts = []  # This is the list to store all contacts
 
     while True:
         show_menu()
         choice = input("Enter your choice: ")
 
         if choice == "5":
-            print("Exiting the program...")
+            print("Exiting the program")
             break
         else:
-            print("Option not yet added. Coming soon!")
+            print("I will implement this feature soon!")
 
 if __name__ == "__main__":
     main()
+
+
+def add_contact(contacts):
+    print("\n---// Adding a New Contact //---")
+    name = input("Enter contact name: ")
+    phone = input("Enter phone number: ")
+    email = input("Enter email address: ")
+
+    # creating dictionary for one contact
+    contact = {
+        "name": name,
+        "phone": phone,
+        "email": email
+    }
+
+    contacts.append(contact)
+    print("Contact is added successfully!")
